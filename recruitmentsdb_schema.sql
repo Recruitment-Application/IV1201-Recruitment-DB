e77f0d79cb1f30bbfb4658359de8d536b21ccd52
Gleano Malke <gleano.100@hotmail.com>
2022-03-02 20:12:43 +0100

    The sql script files to create the database
    Two scripts, one of them used to create the schema of the database and the second one is create the whole databse including the data.

---
+++ recruitmentsdb_schema.sql
@@ -0,0 +1,504 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.2
+-- Dumped by pg_dump version 13.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: applicant_availability; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.applicant_availability (
+    id integer NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL,
+    person_id integer NOT NULL,
+    application_id integer
+);
+
+
+ALTER TABLE public.applicant_availability OWNER TO postgres;
+
+--
+-- Name: applicant_availability_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.applicant_availability_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.applicant_availability_id_seq OWNER TO postgres;
+
+--
+-- Name: applicant_availability_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.applicant_availability_id_seq OWNED BY public.applicant_availability.id;
+
+
+--
+-- Name: application; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.application (
+    id integer NOT NULL,
+    years_of_experience numeric(3,2) NOT NULL,
+    competence_id integer NOT NULL,
+    person_id integer NOT NULL
+);
+
+
+ALTER TABLE public.application OWNER TO postgres;
+
+--
+-- Name: application_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.application_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.application_id_seq OWNER TO postgres;
+
+--
+-- Name: application_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.application_id_seq OWNED BY public.application.id;
+
+
+--
+-- Name: application_status; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.application_status (
+    id integer NOT NULL,
+    decision character varying(50) NOT NULL,
+    recruiter_id integer,
+    application_id integer NOT NULL
+);
+
+
+ALTER TABLE public.application_status OWNER TO postgres;
+
+--
+-- Name: application_status_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.application_status_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.application_status_id_seq OWNER TO postgres;
+
+--
+-- Name: application_status_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.application_status_id_seq OWNED BY public.application_status.id;
+
+
+--
+-- Name: competence; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.competence (
+    id integer NOT NULL,
+    type character varying(100) NOT NULL,
+    job_id integer NOT NULL
+);
+
+
+ALTER TABLE public.competence OWNER TO postgres;
+
+--
+-- Name: competence_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.competence_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.competence_id_seq OWNER TO postgres;
+
+--
+-- Name: competence_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.competence_id_seq OWNED BY public.competence.id;
+
+
+--
+-- Name: job; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.job (
+    id integer NOT NULL,
+    name character varying(100) NOT NULL
+);
+
+
+ALTER TABLE public.job OWNER TO postgres;
+
+--
+-- Name: job_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.job_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.job_id_seq OWNER TO postgres;
+
+--
+-- Name: job_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.job_id_seq OWNED BY public.job.id;
+
+
+--
+-- Name: login_info; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.login_info (
+    id integer NOT NULL,
+    email character varying(100),
+    password character varying(500),
+    username character varying(100),
+    person_id integer NOT NULL
+);
+
+
+ALTER TABLE public.login_info OWNER TO postgres;
+
+--
+-- Name: login_info_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.login_info_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.login_info_id_seq OWNER TO postgres;
+
+--
+-- Name: login_info_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.login_info_id_seq OWNED BY public.login_info.id;
+
+
+--
+-- Name: person; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.person (
+    id integer NOT NULL,
+    first_name character varying(100) NOT NULL,
+    last_name character varying(100) NOT NULL,
+    personal_number character varying(13),
+    role_id integer NOT NULL
+);
+
+
+ALTER TABLE public.person OWNER TO postgres;
+
+--
+-- Name: person_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.person_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.person_id_seq OWNER TO postgres;
+
+--
+-- Name: person_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.person_id_seq OWNED BY public.person.id;
+
+
+--
+-- Name: role; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.role (
+    id integer NOT NULL,
+    type character varying(100) NOT NULL
+);
+
+
+ALTER TABLE public.role OWNER TO postgres;
+
+--
+-- Name: role_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.role_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.role_id_seq OWNER TO postgres;
+
+--
+-- Name: role_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.role_id_seq OWNED BY public.role.id;
+
+
+--
+-- Name: applicant_availability id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.applicant_availability ALTER COLUMN id SET DEFAULT nextval('public.applicant_availability_id_seq'::regclass);
+
+
+--
+-- Name: application id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.application ALTER COLUMN id SET DEFAULT nextval('public.application_id_seq'::regclass);
+
+
+--
+-- Name: application_status id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.application_status ALTER COLUMN id SET DEFAULT nextval('public.application_status_id_seq'::regclass);
+
+
+--
+-- Name: competence id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.competence ALTER COLUMN id SET DEFAULT nextval('public.competence_id_seq'::regclass);
+
+
+--
+-- Name: job id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.job ALTER COLUMN id SET DEFAULT nextval('public.job_id_seq'::regclass);
+
+
+--
+-- Name: login_info id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.login_info ALTER COLUMN id SET DEFAULT nextval('public.login_info_id_seq'::regclass);
+
+
+--
+-- Name: person id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.person ALTER COLUMN id SET DEFAULT nextval('public.person_id_seq'::regclass);
+
+
+--
+-- Name: role id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role ALTER COLUMN id SET DEFAULT nextval('public.role_id_seq'::regclass);
+
+
+--
+-- Name: applicant_availability applicant_availability_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.applicant_availability
+    ADD CONSTRAINT applicant_availability_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: job available_job_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.job
+    ADD CONSTRAINT available_job_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: application job_application_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.application
+    ADD CONSTRAINT job_application_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: application_status job_application_status_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.application_status
+    ADD CONSTRAINT job_application_status_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: competence job_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.competence
+    ADD CONSTRAINT job_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: login_info login_info_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.login_info
+    ADD CONSTRAINT login_info_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: person peson_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT peson_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: role role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role
+    ADD CONSTRAINT role_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: application_status application_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.application_status
+    ADD CONSTRAINT application_id FOREIGN KEY (application_id) REFERENCES public.application(id);
+
+
+--
+-- Name: applicant_availability application_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.applicant_availability
+    ADD CONSTRAINT application_id FOREIGN KEY (application_id) REFERENCES public.application(id) NOT VALID;
+
+
+--
+-- Name: application competence_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.application
+    ADD CONSTRAINT competence_id FOREIGN KEY (competence_id) REFERENCES public.competence(id);
+
+
+--
+-- Name: competence job_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.competence
+    ADD CONSTRAINT job_id FOREIGN KEY (job_id) REFERENCES public.job(id);
+
+
+--
+-- Name: login_info person_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.login_info
+    ADD CONSTRAINT person_id FOREIGN KEY (person_id) REFERENCES public.person(id);
+
+
+--
+-- Name: applicant_availability person_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.applicant_availability
+    ADD CONSTRAINT person_id FOREIGN KEY (person_id) REFERENCES public.person(id);
+
+
+--
+-- Name: application person_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.application
+    ADD CONSTRAINT person_id FOREIGN KEY (person_id) REFERENCES public.person(id);
+
+
+--
+-- Name: application_status person_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.application_status
+    ADD CONSTRAINT person_id FOREIGN KEY (recruiter_id) REFERENCES public.person(id);
+
+
+--
+-- Name: person role_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT role_id FOREIGN KEY (role_id) REFERENCES public.role(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
